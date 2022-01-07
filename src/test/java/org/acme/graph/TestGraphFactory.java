f86102c3c1c392da8d8a253fe741b0e2b2d69b33
antoine-overflow <antoine@DESKTOP-VKFREPH.localdomain>
2022-01-07 14:30:01 +0100

    0.3 - Géométrie réelle des tronçons

--- src/test/java/org/acme/graph/TestGraphFactory.java
+++ src/test/java/org/acme/graph/TestGraphFactory.java
@@ -16,26 +16,18 @@ public class TestGraphFactory {
 		Graph graph = new Graph();
 
 		Vertex a = graph.createVertex(new Coordinate(0.0, 0.0), "a");
-		graph.getVertices().add(a);
 
 		Vertex b = graph.createVertex(new Coordinate(1.0, 0.0), "b");
-		graph.getVertices().add(b);
 
 		Vertex c = graph.createVertex(new Coordinate(2.0, 0.0), "c");
-		graph.getVertices().add(c);
 
 		Vertex d = graph.createVertex(new Coordinate(1.0, 1.0), "d");
-		graph.getVertices().add(d);
 
 		Edge ab = graph.createEdge(a, b, "ab");
-		graph.getEdges().add(ab);
 
 		Edge bc = graph.createEdge(b, c, "bc");
-		graph.getEdges().add(bc);
 
 		Edge ad = graph.createEdge(a, d, "ad");
-		ad.setId("ad");
-		graph.getEdges().add(ad);
 
 		return graph;
 	}
